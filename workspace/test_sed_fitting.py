0c0ee0117c361763bf99922926df248f10b19854
danielsf <scott.f.daniel@gmail.com>
2017-11-07 13:59:18 -0800

    test sed fitting on actual catalog data

--- workspace/test_sed_fitting.py
+++ workspace/test_sed_fitting.py
@@ -1,23 +1,55 @@
 import numpy as np
 import os
 
-rng = np.random.RandomState(8123)
-
-n_stars = 1000
-n_mags = 30
-
-random_mags = rng.random_sample((n_mags, n_stars))
-
+from GCRCatalogs import load_catalog
 from GCRCatalogs import sed_from_galacticus_mags
 
-sed_names, mag_norms = sed_from_galacticus_mags(random_mags)
+from lsst.utils import getPackageDir
+
+catalog = load_catalog('proto-dc2_v2.0')
+
+qty_names = []
+with open(os.path.join(getPackageDir('gcr_catalogs'), 'CatSimSupport', 'CatSimMagGrid.txt'), 'r') as in_file:
+    header = in_file.readlines()[0]
+
+header = header.strip().split()
+disk_mag_names = []
+bulge_mag_names = []
+
+for name in header[2:-1]:
+    disk_mag_names.append('SEDs/diskLuminositiesStellar:SED_%s:rest' % name)
+    bulge_mag_names.append('SEDs/spheroidLuminositiesStellar:SED_%s:rest' % name)
+
+for name in disk_mag_names:
+    qty_names.append(name)
+
+for name in bulge_mag_names:
+    qty_names.append(name)
+
+qty_names.append('morphology/diskRadiusArcsec')
+qty_names.append('morphology/spheroidRadiusArcsec')
+
+catalog_qties = catalog.get_quantities(qty_names)
+
+has_disk = np.where(catalog_qties['morphology/diskRadiusArcsec']>0.0)
+has_spheroid = np.where(catalog_qties['morphology/spheroidRadiusArcsec']>0.0)
+
+first_disk = has_disk[0][:10000]
+
+disk_mags = np.array([-2.5*np.log10(catalog_qties[name][first_disk]) for name in disk_mag_names])
+
+import time
+t_start = time.time()
+sed_names, mag_norms = sed_from_galacticus_mags(disk_mags)
+print("fitting %d took %.3e" % (len(sed_names), time.time()-t_start))
+assert len(sed_names) == len(first_disk)
+
 
 dtype_list = [('name', str, 200)]
 for ii in range(30):
     dtype_list.append(('mag%d' % ii, float))
 dtype_list.append(('magNorm', float))
 
-from lsst.utils import getPackageDir
 
 catsim_name = os.path.join(getPackageDir('gcr_catalogs'), 'CatSimSupport',
                            'CatSimMagGrid.txt')
@@ -27,7 +59,7 @@ dtype = np.dtype(dtype_list)
 sed_data = np.genfromtxt(catsim_name, dtype=dtype)
 
 worst_dist = -1.0
-for i_star in range(n_stars):
+for i_star in range(len(sed_names)):
     chosen_sed = None
     for i_sed in range(len(sed_data)):
         if sed_data['name'][i_sed] == sed_names[i_star]:
@@ -40,7 +72,7 @@ for i_star in range(n_stars):
 
     dist = 0.0
     for i_mag in range(30):
-        dist += (random_mags[i_mag][i_star] - sed_data['mag%d' % i_mag][chosen_sed] - d_mag)**2
+        dist += (disk_mags[i_mag][i_star] - sed_data['mag%d' % i_mag][chosen_sed] - d_mag)**2
     dist = np.sqrt(dist)
     if dist> worst_dist:
         worst_dist = dist
