1b7e8148327481788beb71de6f8580e71d217b4c
danielsf <scott.f.daniel@gmail.com>
2017-11-14 16:35:09 -0800

    need to re-order imports

--- GCRCatalogs/__init__.py
+++ GCRCatalogs/__init__.py
@@ -8,8 +8,8 @@ from .alphaq import *
 from .buzzard import *
 from .dc1 import *
 
+from .gcrCatSimSedFitter import *
 from .gcrCatSimDatabaseEmulator import *
 from .gcrCatSimCatalogClasses import *
-from .gcrCatSimSedFitter import *
 
 from .version import __version__
