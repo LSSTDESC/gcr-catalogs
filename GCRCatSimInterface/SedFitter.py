30566b1be1cf1ba65782a0bcacf2731048aac0e2
danielsf <scott.f.daniel@gmail.com>
2017-11-14 22:13:28 -0800

    redirect to sed script

--- GCRCatSimInterface/SedFitter.py
+++ GCRCatSimInterface/SedFitter.py
@@ -27,7 +27,7 @@ def sed_from_galacticus_mags(galacticus_mags, redshift, h=0.71, omega_m=0.265):
 
     if not hasattr(sed_from_galacticus_mags, '_sed_color_tree'):
         catsim_dir = os.path.join(getPackageDir('gcr_catalogs'),
-                                  'CatSimSupport')
+                                  'GCRCatSimInterface', 'data')
         color_grid_file = os.path.join(catsim_dir, 'CatSimMagGrid.txt')
 
         if not os.path.exists(color_grid_file):
