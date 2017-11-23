f3167383bb8b15e971f36f39273d7a3359eb1129
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-11-23 16:04:47 -0500

    Using a new import method that does not require all readers to be imported when import top-level package (#25)
    * adopt new import method
    
    * clean up dependencies
    
    * update test

--- GCRCatalogs/__init__.py
+++ GCRCatalogs/__init__.py
@@ -1,11 +1,5 @@
 import GCR
 from GCR import *
 
-from .utils import *
 from .register import *
-
-from .alphaq import *
-from .buzzard import *
-from .dc1 import *
-
 from .version import __version__
--- GCRCatalogs/register.py
+++ GCRCatalogs/register.py
@@ -1,65 +1,110 @@
 import os
+import importlib
+import yaml
 from GCR import BaseGenericCatalog
-from .utils import load_yaml
 
-_registered_readers = dict()
-_registered_catalogs = dict()
 
-__all__ = ['register_reader', 'register_catalog', 'get_available_readers', 'get_available_catalogs', 'load_catalog']
+__all__ = ['available_catalogs', 'get_available_catalogs', 'load_catalog']
 
-def register_reader(subclass):
+
+def load_yaml(yaml_file):
     """
-    Registers a new galaxy catalog type with the loading utility.
-
-    Parameters
-    ----------
-    subclass: subclass of BaseGenericCatalog
+    Load *yaml_file*. Ruturn a dictionary.
     """
-    assert issubclass(subclass, BaseGenericCatalog), "Provided class is not a subclass of BaseGenericCatalog"
-    _registered_readers[subclass.__name__] = subclass
+    with open(yaml_file) as f:
+        config = yaml.load(f)
+    return config
 
 
-def register_catalog(catalog_name, catalog_config):
+def import_subclass(subclass_path, package=None, required_base_class=None):
     """
-    Registers a new galaxy catalog with the config file.
-
-    Parameters
-    ----------
-    catalog_name
+    Import and return a subclass.
+    *subclass_path* must be in the form of 'module.subclass'.
     """
-    _registered_catalogs[catalog_name] = catalog_config
+    module, _, subclass_name = subclass_path.rpartition('.')
+    if package and not module.startswith('.'):
+        module = '.' + module
+    subclass = getattr(importlib.import_module(module, package), subclass_name)
+    if required_base_class:
+        assert issubclass(subclass, required_base_class), "Provided class is not a subclass of *required_base_class*"
+    return subclass
 
 
-for _ in os.listdir(os.path.join(os.path.dirname(__file__), 'catalog_configs')):
-    register_catalog(os.path.splitext(_)[0], load_yaml(os.path.join(os.path.dirname(__file__), 'catalog_configs', _)))
+def get_available_configs(config_dir, register=None):
+    """
+    Return (or update) a dictionary *register* that contains all config files in *config_dir*.
+    """
+    if register is None:
+        register = dict()
+
+    for config_file in os.listdir(config_dir):
+        if config_file.startswith('_') or not config_file.lower().endswith('.yaml'):
+            continue
+
+        name = os.path.splitext(config_file)[0]
+        config = load_yaml(os.path.join(config_dir, config_file))
+        register[name] = config
+
+    return register
 
 
 def get_available_catalogs():
-    return _registered_catalogs
+    """
+    Return *available_catalogs* as a dictionary
+    """
+    return available_catalogs
 
 
-def get_available_readers():
-    return _registered_readers
+def load_catalog_from_config_dict(catalog_config):
+    """
+    Load a galaxy catalog using a config dictionary.
+
+    Parameters
+    ----------
+    catalog_config : dict
+        a dictionary of config options
+
+    Return
+    ------
+    galaxy_catalog : instance of a subclass of BaseGalaxyCatalog
+
+    See also
+    --------
+    load_catalog()
+    """
+    return import_subclass(catalog_config['subclass_name'],
+                           __package__,
+                           BaseGenericCatalog)(**catalog_config)
 
 
 def load_catalog(catalog_name, config_overwrite=None):
     """
-    Load a galaxy catalog as specified in a yaml config file.
+    Load a galaxy catalog as specified in one of the yaml file in catalog_configs.
 
     Parameters
     ----------
     catalog_name : str
-        name of the catalog
+        name of the catalog (without '.yaml')
     config_overwrite : dict, optional
         a dictionary of config options to overwrite
 
     Return
     ------
-    galaxy_catalog : subclass of BaseGalaxyCatalog
+    galaxy_catalog : instance of a subclass of BaseGalaxyCatalog
     """
-    if catalog_name not in _registered_catalogs:
-        raise KeyError("Catalog `{}` does not exist in the register (catalog_configs)".format(catalog_name))
-    config = _registered_catalogs[catalog_name]
+    if catalog_name.lower().endswith('.yaml'):
+        catalog_name = catalog_name[:-5]
+
+    if catalog_name not in available_catalogs:
+        raise KeyError("Catalog `{}` does not exist in the register. See `available_catalogs`.".format(catalog_name))
+
+    config = available_catalogs[catalog_name]
+
     if config_overwrite:
+        config = config.copy()
         config.update(config_overwrite)
-    return _registered_readers[config['subclass_name']](**config)
+
+    return load_catalog_from_config_dict(config)
+
+
+available_catalogs = get_available_configs(os.path.join(os.path.dirname(__file__), 'catalog_configs'))
