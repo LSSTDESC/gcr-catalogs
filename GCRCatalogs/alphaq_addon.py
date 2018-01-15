500aa32d2d87c1f9d99335e7d821801c3f86c458
Francois <francois@orioncloud.fr>
2018-01-14 23:49:55 -0500

    Added generic alphaQ addon format

--- GCRCatalogs/alphaq_addon.py
+++ GCRCatalogs/alphaq_addon.py
@@ -10,13 +10,55 @@ from GCR import BaseGenericCatalog
 
 from .alphaq import AlphaQGalaxyCatalog
 
-try:
-  import tensorflow as tf
-  HAS_TENSORFLOW = True
-except ImportError:
-  HAS_TENSORFLOW = False
+__all__ = ['AlphaQTidalCatalog', 'AlphaAddonCatalog']
 
-__all__ = ['AlphaQTidalCatalog', 'AlphaQKnotsCatalog']
+
+class AlphaQAddonCatalog(AlphaQGalaxyCatalog):
+    """
+    Addon to the AlphaQ catalog that can add extra quanities to the baseline
+    catalog
+    """
+    def _subclass_init(self, **kwargs):
+        # Loads main catalog
+        super(self.__class__, self)._subclass_init(**kwargs)
+
+        # Sets the filename of the addon
+        self._addon_filename = kwargs['addon_filename']
+        assert os.path.isfile(self._addon_filename), 'Addon file {} does not exist'.format(self._addon_filename)
+        self._addon_group = kwargs['addon_group']
+
+    def _generate_native_quantity_list(self):
+        # Generates the native quantity list for the parent catalog
+        native_quantities = super(self.__class__, self)._generate_native_quantity_list()
+
+        # Loads the additional data provided by the addon file
+        with h5py.File(self._addon_filename, 'r') as fh:
+            hgroup = fh[self._addon_group]
+            hobjects = []
+            #get all the names of objects in this tree
+            hgroup.visit(hobjects.append)
+            #filter out the group objects and keep the dataste objects
+            hdatasets = [hobject for hobject in hobjects if type(hgroup[hobject]) == h5py.Dataset]
+            addon_native_quantities = set(hdatasets)
+
+        self._addon_native_quantities = addon_native_quantities
+
+        return native_quantities + addon_native_quantities
+
+    def _iter_native_dataset(self, native_filters=None):
+        """
+        Caution, fully overiddes parent function
+        """
+        assert not native_filters, '*native_filters* is not supported'
+        with h5py.File(self._file, 'r') as fh:
+            with h5py.File(self._addon_filename, 'r') as fh_addon:
+                def native_quantity_getter(native_quantity):
+                    if native_quantity in self._addon_native_quantities:
+                        return fh_addon['{}/{}'.format(self._addon_group,native_quantity)].value
+                    else:
+                        return fh['galaxyProperties/{}'.format(native_quantity)].value
+                yield native_quantity_getter
+
 
 class AlphaQTidalCatalog(BaseGenericCatalog):
     """
@@ -62,60 +104,3 @@ class AlphaQTidalCatalog(BaseGenericCatalog):
                 cols = (slice(None),) + tuple((int(i) for i in items[1:]))
                 return data[name][cols]
             yield native_quantity_getter
-
-
-class AlphaQKnotsCatalog(AlphaQGalaxyCatalog):
-    """
-    Addon to the AlphaQ catalog that adds a RandomWalk component to the galaxy
-    """
-
-    def _subclass_init(self, **kwargs):
-        super(self.__class__, self)._subclass_init(**kwargs)
-
-        # Adds additional field required by the sampling code
-        self._quantity_modifiers['morphology/BTR'] = (lambda x,y : x/(x+y), 'SDSS_filters/spheroidLuminositiesStellar:SDSS_i:observed', 'SDSS_filters/diskLuminositiesStellar:SDSS_i:observed')
-
-        if not HAS_TENSORFLOW:
-            raise TypeError('The RandomWalk catalog requires tensorflow')
-
-        self._model_dir = kwargs['model_dir']
-        self._function_name = kwargs['function_name']
-        self._batch_size = 10000
-        assert os.path.exists(self._model_dir), 'Model directory {} does not exist'.format(self._model_dir)
-
-        # Opens tf Session
-        self._sess = tf.Session()
-
-        # Load the saved tensorflow model
-        model = tf.saved_model.loader.load(self._sess, tags=['serve'], export_dir=self._model_dir)
-
-        # Extracts the signature definition of the function to use
-        definition = model.signature_def[self._function_name]
-
-        requested_quantities = tuple([n for n in definition.inputs])
-        generated_quantities = tuple([n for n in definition.outputs])
-
-        self._native_quantities = []
-
-        # Define the serving function
-        def get_function_mapper(output_name):
-
-            def func(*x):
-                x_size = len(x[0])
-                n_batches = x_size // self._batch_size
-                n_batches += 0 if (x_size % self._batch_size) == 0 else 1
-
-                # list storing the results
-                res = []
-                # Create data dictionary
-                feed_dict = {}
-                for b in range(n_batches):
-                    for i, n in enumerate(definition.inputs):
-                        feed_dict[definition.inputs[n].name] = x[i][b*self._batch_size:min((b+1)*self._batch_size, x_size)].astype('float32')
-                    res.append(self._sess.run(definition.outputs[output_name].name, feed_dict=feed_dict))
-                return np.concatenate(res)
-            return func
-
-        # Add the quantity modifier
-        for quantity in generated_quantities:
-            self.add_modifier_on_derived_quantities(quantity, get_function_mapper( quantity), *requested_quantities )
