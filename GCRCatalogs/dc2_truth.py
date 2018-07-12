1138a55899cb58ccf866a483c9b29eee7a6a31a4
danielsf <scott.f.daniel@gmail.com>
2018-07-12 16:02:09 -0700

    only load requested quantities (in the simplest way imaginable)

--- GCRCatalogs/dc2_truth.py
+++ GCRCatalogs/dc2_truth.py
@@ -32,41 +32,30 @@ class DC2TruthCatalogReader(BaseGenericCatalog):
     def _iter_native_dataset(self, native_filters=None):
         cursor = self._conn.cursor()
 
-        column_list = list(self._native_quantities)
-        query = 'SELECT '
-        for column_name in column_list:
-            if query != 'SELECT ':
-                query += ', '
-            query += column_name
-
-        query += ' FROM truth'
+        query_where_clause = ' FROM truth'
 
         if native_filters is not None:
-            query += ' WHERE'
+            query_where_clause += ' WHERE'
 
             if not isinstance(native_filters, list) and not isinstance(native_filters, tuple):
                 native_filters = [native_filters]
 
             for i_filt, filt in enumerate(native_filters):
                 if i_filt > 0:
-                    query += ' AND'
-                query += ' {}'.format(filt)
-
-        query_cursor = cursor.execute(query)
-
-        # when we transition to CosmoDC2, this would be a place
-        # to use fetchmany(chunk_size) to iterate over manageable
-        # chunks of the catalog, if necessary
-        query_result = np.array(query_cursor.fetchall()).transpose()
-
-        out_dict = {}
-        for i_col, column_name in enumerate(column_list):
-            out_dict[column_name] = query_result[i_col]
+                    query_where_clause += ' AND'
+                query_where_clause += ' {}'.format(filt)
 
         # define a method to return a native_quantity_getter
         # with the API expected by the GCR
         def dc2_truth_native_quantity_getter(quantity):
-            return out_dict[quantity]
+            query = 'SELECT %s' % quantity
+            query += query_where_clause
+            query_cursor = cursor.execute(query)
+
+            # when we transition to CosmoDC2, this would be a place
+            # to use fetchmany(chunk_size) to iterate over manageable
+            # chunks of the catalog, if necessary
+            return np.array(query_cursor.fetchall()).flatten()
 
         yield dc2_truth_native_quantity_getter
 
