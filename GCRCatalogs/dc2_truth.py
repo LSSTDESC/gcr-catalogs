32ccbd237d1d144a34511c99e57c15cd1a1480a3
danielsf <scott.f.daniel@gmail.com>
2018-07-11 22:45:12 -0700

    add _iter_native_dataset

--- GCRCatalogs/dc2_truth.py
+++ GCRCatalogs/dc2_truth.py
@@ -18,3 +18,35 @@ class DC2TruthCatalogReader(BaseGenericCatalog):
         cursor = self._conn.cursor()
         results = cursor.execute("PRAGMA table_info('truth')").fetchall()
         return [r[1] for r in results]
+
+    def _iter_native_dataset(self, native_filters=None):
+        cursor = self._conn.cursor()
+
+        column_list = list(self._native_quantities)
+        query = 'SELECT '
+        for column_name in column_list:
+            if query != 'SELECT ':
+                query += ', '
+            query += column_name
+
+        query += ' FROM truth'
+
+        if native_filters is not None:
+            query += ' WHERE'
+
+            if not isintance(native_filters, list) and not isinstance(native_filters, tuple):
+                native_filters = [native_filters]
+
+            for i_filt, filt in enumerate(native_filters):
+                if i_filt > 0:
+                    query += ' AND'
+                query += ' {}'.format(filt)
+
+        query_cursor = cursor.execute(query)
+        query_result = np.array(query_cursor.fetchall()).transpose()
+
+        out_dict = {}
+        for i_col, column_name in enumerate(column_list):
+            out_dict[column_name] = query_result[i_col]
+
+        yield out_dict
