359e334887842b66da0d766c4b8f2f2909725b35
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-10-05 11:34:31 -0400

    add DC2TruthCatalogLightCurveReader

--- GCRCatalogs/dc2_truth.py
+++ GCRCatalogs/dc2_truth.py
@@ -4,7 +4,7 @@ import numpy as np
 from GCR import BaseGenericCatalog
 from .utils import md5, is_string_like
 
-__all__ = ["DC2TruthCatalogReader"]
+__all__ = ['DC2TruthCatalogReader', 'DC2TruthCatalogLightCurveReader']
 
 
 class DC2TruthCatalogReader(BaseGenericCatalog):
@@ -15,6 +15,10 @@ class DC2TruthCatalogReader(BaseGenericCatalog):
     ----------
     filename : str
         path to the sqlite database file
+    table_name : str
+        table name
+    is_static : bool
+        whether or not this is for static objects only
     base_filters : str or list of str, optional
         set of filters to always apply to the where clause
     """
@@ -23,7 +27,10 @@ class DC2TruthCatalogReader(BaseGenericCatalog):
 
     def _subclass_init(self, **kwargs):
         self._filename = kwargs['filename']
+
         self._table_name = kwargs.get('table_name', 'truth')
+        self._is_static = kwargs.get('is_static', True)
+
         base_filters = kwargs.get('base_filters')
         if base_filters:
             if is_string_like(base_filters):
@@ -43,7 +50,7 @@ class DC2TruthCatalogReader(BaseGenericCatalog):
 
         # get the descriptions of the columns as provided in the sqlite database
         cursor = self._conn.cursor()
-        if self._table_name == 'truth':
+        if self._is_static:
             results = cursor.execute('SELECT name, description FROM column_descriptions')
             self._column_descriptions = dict(results.fetchall())
         else:
@@ -52,7 +59,7 @@ class DC2TruthCatalogReader(BaseGenericCatalog):
         results = cursor.execute("PRAGMA table_info('{}')".format(self._table_name))
         self._native_quantity_dtypes = {t[1]: t[2] for t in results.fetchall()}
 
-        if self._table_name == 'truth':
+        if self._is_static:
             self._quantity_modifiers = {
                 'mag_true_u': 'u',
                 'mag_true_g': 'g',
@@ -107,3 +114,103 @@ class DC2TruthCatalogReader(BaseGenericCatalog):
         if quantity in self._column_descriptions:
             return {'description': self._column_descriptions[quantity]}
         return default
+
+
+class DC2TruthCatalogLightCurveReader(BaseGenericCatalog):
+    """
+    DC2 truth catalog reader for light curves
+
+    Parameters
+    ----------
+    filename : str
+        path to the sqlite database file
+    table_light_curves : str
+        light curve table name
+    table_summary : str
+        summary table name
+    table_obs_metadata : str
+        observation metadata table name
+    base_filters : str or list of str, optional
+        set of filters to always apply to the where clause
+    """
+
+    native_filter_string_only = True
+
+    def _subclass_init(self, **kwargs):
+        self._filename = kwargs['filename']
+
+        self._tables = dict()
+        self._tables['light_curves'] = kwargs.get('table_light_curves', 'light_curves')
+        self._tables['summary'] = kwargs.get('table_summary', 'variables_and_transients')
+        self._tables['obs_meta'] = kwargs.get('table_obs_metadata', 'obs_metadata')
+
+        base_filters = kwargs.get('base_filters')
+        if base_filters:
+            if is_string_like(base_filters):
+                self.base_filters = (base_filters,)
+            else:
+                self.base_filters = tuple(base_filters)
+        else:
+            self.base_filters = tuple()
+
+        if not os.path.isfile(self._filename):
+            raise ValueError('{} is not a valid file'.format(self._filename))
+
+        if kwargs.get('md5') and md5(self._filename) != kwargs['md5']:
+            raise ValueError('md5 sum does not match!')
+
+        self._conn = sqlite3.connect(self._filename)
+        cursor = self._conn.cursor()
+        self._dtypes = dict()
+        for table, table_name in self._tables.items():
+            if table == 'obs_meta':
+                table = 'light_curves'
+            results = cursor.execute("PRAGMA table_info('{}')".format(table_name))
+            self._dtypes[table] = {t[1]: t[2] for t in results.fetchall()}
+
+    def _generate_native_quantity_list(self):
+        return list(self._tables['light_curves'])
+
+    @staticmethod
+    def _obtain_native_data_dict(native_quantities_needed, native_quantity_getter):
+        """
+        Overloading this so that we can query the database backend
+        for multiple columns at once
+        """
+        return native_quantity_getter(native_quantities_needed)
+
+    def _iter_native_dataset(self, native_filters=None):
+        cursor = self._conn.cursor()
+
+        if native_filters is not None:
+            all_filters = self.base_filters + tuple(native_filters)
+        else:
+            all_filters = self.base_filters
+
+        if all_filters:
+            query_where_clause = 'WHERE {}'.format(' AND '.join(all_filters))
+        else:
+            query_where_clause = ''
+
+        id_col_name = 'uniqueId'
+        dtype = np.dtype([(id_col_name, self._dtypes['summary'][id_col_name])])
+        query = 'SELECT DISTINCT {} FROM {} {}'.format(
+            id_col_name,
+            self._tables['summary'],
+            query_where_clause
+        )
+        ids_needed = np.array(cursor.execute(query).fetchall(), dtype)[id_col_name]
+
+        for id_this in ids_needed:
+            def dc2_truth_light_curve_native_quantity_getter(quantities):
+                dtype = np.dtype([(q, self._dtypes['light_curves'][q]) for q in quantities])
+                query = 'SELECT {0} FROM {1} LEFT JOIN {2} ON {1}.{4}={5} AND {1}.{3}={2}.{3}'.format(
+                    ', '.join(quantities),
+                    self._tables['light_curves'],
+                    self._tables['obs_meta'],
+                    'obshistid',
+                    id_col_name,
+                    id_this
+                )
+                return np.array(cursor.execute(query).fetchall(), dtype)
+            yield dc2_truth_light_curve_native_quantity_getter
