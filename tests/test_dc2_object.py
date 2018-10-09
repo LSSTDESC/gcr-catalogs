64319fff963e3bfe8d752734be115f19478502be
Michael Wood-Vasey <wmwv@pitt.edu>
2018-10-09 10:12:41 -0400

    Test getting tract, patch constant arrays.

--- tests/test_dc2_object.py
+++ tests/test_dc2_object.py
@@ -30,3 +30,21 @@ def test_get_missing_column(load_dc2_catalog):
                        np.repeat(-1, len(gc)))
     assert_array_equal(empty_bool_column_should_be_False,
                        np.repeat(False, len(gc)))
+
+
+def test_get_tract_patch(load_dc2_catalog):
+    """Verify that we get tract, patch columns correctly.
+
+    These are not originally stored as columns in the HDF5 files.
+    They are added by the reader.
+    So we want to test here that that actually works.
+    """
+    gc = load_dc2_catalog
+    tract = 4850
+    patch = '3,1'
+
+    tract_col = gc['tract']
+    patch_col = gc['patch']
+
+    assert_array_equal(tract_col, np.repeat(tract, len(gc)))
+    assert_array_equal(patch_col, np.repeat(patch, len(gc)))
