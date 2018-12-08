1e8ba090ed42b32c2b7cfed2b3227611e093d76e
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-12-08 11:57:53 -0500

    update GCR version requirement

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     ],
     keywords='GCR',
     packages=['GCRCatalogs'],
-    install_requires=['future', 'requests', 'pyyaml', 'numpy', 'astropy', 'GCR>=0.8.3'],
+    install_requires=['future', 'requests', 'pyyaml', 'numpy', 'astropy', 'GCR>=0.8.7'],
     extras_require={
         'protodc2': ['h5py'],
         'cosmodc2': ['h5py', 'healpy'],
