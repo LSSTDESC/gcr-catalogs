c533c18e38a9cd4806cd08243f39bb99432395fb
Eli Rykoff <erykoff@stanford.edu>
2018-12-13 15:04:36 -0800

    Add support for new RedmapperCatalog (python) and the run on cosmoDC2_v1.1.4

--- GCRCatalogs/redmapper.py
+++ GCRCatalogs/redmapper.py
@@ -9,7 +9,7 @@ from astropy.io import fits
 from astropy.cosmology import FlatLambdaCDM
 from GCR import BaseGenericCatalog
 
-__all__ = ['RedMapperLegacyCatalog']
+__all__ = ['RedmapperCatalog', 'RedMapperLegacyCatalog']
 
 
 class FitsFile(object):
@@ -23,6 +23,108 @@ class FitsFile(object):
         del self._file_handle[1].data #pylint: disable=E1101
         self._file_handle.close()
         del self._file_handle
+
+class RedmapperCatalog(BaseGenericCatalog):
+    """
+    redMaPPer cluster catalog class.  Uses generic quantity and filter mechanisms
+    defined by BaseGenericCatalog class.
+    """
+
+    def _subclass_init(self, catalog_root_dir,
+                       catalog_path_template,
+                       use_cache=True,
+                       **kwargs): #pylint: disable=W0221
+
+        if not os.path.isdir(catalog_root_dir):
+            raise RuntimeError("Catalog directory %s does not exist." % (catalog_root_dir))
+
+        self._catalog_path_template = {k: os.path.join(catalog_root_dir, v) for k, v in catalog_path_template.items()}
+
+        cosmology = kwargs.get('cosmology', {})
+        cosmo_astropy_allowed = FlatLambdaCDM.__init__.__code__.co_varnames[1:]
+        cosmo_astropy = {k: v for k, v in cosmology.items() if k in cosmo_astropy_allowed}
+        self.cosmology = FlatLambdaCDM(**cosmo_astropy)
+        for k, v in cosmology.items():
+            if k not in cosmo_astropy_allowed:
+                setattr(self.cosmology, k, v)
+
+        self.sky_area = kwargs.get('sky_area')
+
+        self.cache = dict() if use_cache else None
+
+        self._quantity_modifiers = {
+            'cluster_id': 'clusters/mem_match_id',
+            'ra': 'clusters/ra',
+            'dec': 'clusters/dec',
+            'redshift': 'clusters/z_lambda',
+            'redshift_err': 'clusters/z_lambda_e',
+            'richness': 'clusters/lambda',
+            'richness_err': 'clusters/lambda_e',
+            'scaleval': 'clusters/scaleval',
+            'redshift_true_cg': 'clusters/cg_spec_z',
+            'maskfrac': 'clusters/maskfrac',
+            'cluster_id_member': 'members/mem_match_id',
+            'ra_member': 'members/ra',
+            'dec_member': 'members/dec',
+            'refmag_member': 'members/refmag',
+            'refmag_err_member': 'members/refmag_err',
+            'redshift_true_member': 'members/zspec',
+            'p_member': 'members/p',
+            'pfree_member': 'members/pfree',
+            'theta_i_member': 'members/theta_i',
+            'theta_r_member': 'members/theta_r'}
+
+        # add centrals
+        for i in range(5):
+            self._quantity_modifiers['ra_cen_%d' % (i)] = 'clusters/ra_cent/%d' % (i)
+            self._quantity_modifiers['dec_cen_%d' % (i)] = 'clusters/dec_cent/%d' % (i)
+            self._quantity_modifiers['p_cen_%d' % (i)] = 'clusters/p_cen/%d' % (i)
+
+        # Add magnitudes
+        for i, band in enumerate(['g', 'r', 'i', 'z', 'y']):
+            self._quantity_modifiers['mag_%s_lsst_member' % (band)] = 'members/mag/%d' % (i)
+            self._quantity_modifiers['magerr_%s_lsst_member' % (band)] = 'members/mag_err/%d' % (i)
+
+    def _iter_native_dataset(self, native_filters=None):
+        if not native_filters:
+            raise RuntimeError("*native_filters* not supported")
+
+        yield functools.partial(self._native_quantity_getter)
+
+    def _generate_native_quantity_list(self):
+        native_quantities = set()
+
+        for subset in self._catalog_path_template.keys():
+            f = self._open_dataset(subset)
+            for name, (dt, _) in f.data.dtype.fields.items():
+                if dt.shape:
+                    for i in range(dt.shape[0]):
+                        native_quantities.add('/'.join((subset, name, str(i))))
+                else:
+                    native_quantities.add('/'.join((subset, name)))
+        return native_quantities
+
+    def _open_dataset(self, subset):
+        path = self._catalog_path_template[subset]
+
+        if self.cache is None:
+            return FitsFile(path)
+
+        key = (subset)
+        if key not in self.cache:
+            self.cache[key] = FitsFile(path)
+        return self.cache[key]
+
+    def _native_quantity_getter(self, native_quantity):
+
+        native_quantity = native_quantity.split('/')
+        assert len(native_quantity) in {2,3}, 'something wrong with the native_quantity {}'.format(native_quantity)
+        subset = native_quantity.pop(0)
+        column = native_quantity.pop(0)
+        data = self._open_dataset(subset).data[column]
+        if native_quantity:
+            data = data[:,int(native_quantity.pop(0))]
+        return data.byteswap().newbyteorder()
 
 
 class RedMapperLegacyCatalog(BaseGenericCatalog):
