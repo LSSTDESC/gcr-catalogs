b2c487876162cdf1b454eae87bdf4ee87a0a14f4
Michael Wood-Vasey <wmwv@pitt.edu>
2019-03-05 22:28:11 -0500

    Start DC2 Source Table Reader.

---
+++ GCRCatalogs/dc2_source.py
@@ -0,0 +1,363 @@
+"""
+DC2 Source Catalog Reader
+"""
+
+import os
+import re
+import warnings
+import itertools
+import shutil
+
+import numpy as np
+import pandas as pd
+import yaml
+from GCR import BaseGenericCatalog
+
+__all__ = ['DC2SourceCatalog']
+
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE_PATTERN = r'source_visit_\d+\.parquet$'
+SCHEMA_FILENAME = 'schema.yaml'
+META_PATH = os.path.join(FILE_DIR, 'catalog_configs/_dc2_object_meta.yaml')
+
+
+def convert_flux_to_nanoJansky(flux, fluxmag0):
+    """Convert the listed DM coadd-reported flux values to nanoJansky.
+
+    Based on the given fluxmag0 value, which is AB mag = 0.
+    Eventually we will get nJy from the final calibrated DRP processing.
+    """
+    AB_mag_zp_wrt_Jansky = 8.90  # Definition of AB
+    AB_mag_zp_wrt_nanoJansky = 2.5 * 9 + AB_mag_zp_wrt_Jansky  # 9 is from nano=10**(-9)
+
+    # AB_mag = -2.5 * np.log10(flux/fluxmag0)
+    # AB_mag = -2.5 * np.log10(calibrated_flux_in_nanoJansky) + AB_mag_zp_wrt_nanoJansky
+    # -2.5 * np.log10(flux/fluxmag0) = -2.5 * np.log10(calibrated_flux_in_nanoJansky) + AB_mag_zp_wrt_nanoJansky
+    # 2.5 * np.log10(calibrated_flux_in_nanoJansky) = 2.5 * np.log10(flux/fluxmag0) + AB_mag_zp_wrt_nanoJansky
+    # calibrated_flux_in_nanoJansky = (flux/fluxmag0) * 10**(AB_mag_zp_wrt_nanoJansky/2.5)
+
+    return 10**((AB_mag_zp_wrt_nanoJansky)/2.5) * flux / fluxmag0
+
+
+def create_basic_flag_mask(*flags):
+    """Generate a mask for a set of flags
+
+    For each item the mask will be true if and only if all flags are false
+
+    Args:
+        *flags (ndarray): Variable number of arrays with booleans or equivalent
+
+    Returns:
+        The combined mask array
+    """
+
+    out = np.ones(len(flags[0]), np.bool)
+    for flag in flags:
+        out &= (~flag)
+
+    return out
+
+
+class DC2SourceCatalog(BaseGenericCatalog):
+    r"""DC2 Source Catalog reader
+
+    Parameters
+    ----------
+    base_dir          (str): Directory of data files being served, required
+    filename_pattern  (str): The optional regex pattern of served data files
+    use_cache        (bool): Whether or not to cache read data in memory
+    is_dpdd          (bool): Whether or not to the files are already in DPDD-format
+
+    Attributes
+    ----------
+    base_dir                     (str): The directory of data files being served
+
+    Notes
+    -----
+    """
+    # pylint: disable=too-many-instance-attributes
+
+    def _subclass_init(self, **kwargs):
+        self.base_dir = kwargs['base_dir']
+        self._filename_re = re.compile(kwargs.get('filename_pattern', FILE_PATTERN))
+        self.use_cache = bool(kwargs.get('use_cache', True))
+
+        if not os.path.isdir(self.base_dir):
+            raise ValueError('`base_dir` {} is not a valid directory'.format(self.base_dir))
+
+        self._datasets = self._generate_datasets()
+        if not self._datasets:
+            err_msg = 'No catalogs were found in `base_dir` {}'
+            raise RuntimeError(err_msg.format(self.base_dir))
+
+        else:
+            # A slightly crude way of checking for version of schema to have modelfit mag
+            # A future improvement will be to explicitly store version information in the datasets
+            # and just rely on that versioning.
+            has_modelfit_mag = any(col.endswith('_modelfit_mag') for col in self._schema)
+
+            if any(col.endswith('_fluxSigma') for col in self._schema):
+                dm_schema_version = 1
+            elif any(col.endswith('_fluxErr') for col in self._schema):
+                dm_schema_version = 2
+            else:
+                dm_schema_version = 3
+
+            self._quantity_modifiers = self._generate_modifiers(
+                    self.pixel_scale, has_modelfit_mag, dm_schema_version)
+
+        self._quantity_info_dict = self._generate_info_dict(META_PATH, bands)
+
+    def __del__(self):
+        self.close_all_file_handles()
+
+    @staticmethod
+    def _generate_modifiers(bands='ugrizy', has_modelfit_mag=True, dm_schema_version=3):
+        """Creates a dictionary relating native and homogenized column names
+
+        Args:
+            has_modelfit_mag (bool): Whether or not pre-calculated model fit magnitudes are present
+            dm_schema_version (int): DM schema version (1, 2, or 3)
+
+        Returns:
+            A dictionary of the form {<homogenized name>: <native name>, ...}
+        """
+
+        if dm_schema_version not in (1, 2, 3):
+            raise ValueError('Only supports dm_schema_version == 1, 2, or 3')
+
+        FLUX = 'flux' if dm_schema_version <= 2 else 'instFlux'
+        ERR = 'Sigma' if dm_schema_version <= 1 else 'Err'
+
+
+        modifiers = {
+            'sourceId': 'id',
+            'parentObjectId': 'parent',
+            'ra': (np.rad2deg, 'coord_ra'),
+            'dec': (np.rad2deg, 'coord_dec'),
+            'x': 'slot_Centroid_x',
+            'y': 'slot_Centroid_y',
+            'xErr': 'slot_Centroid_x{}'.format(ERR),
+            'yErr': 'slot_Centroid_y{}'.format(ERR),
+            'xy_flag': 'slot_Centroid_flag',
+            'sky': (convert_flux_to_nanoJansky, 'base_LocalBackground_{}'.format(FLUX)),
+            'skyErr': (convert_flux_to_nanoJansky, 'base_LocalBackground_{}{}'.format(FLUX, ERR)),
+            'sky_flag': 'base_LocalBackground_flag',
+            'I_flag': 'slot_Shape_flag',
+            'Ixx': 'slot_Shape_xx',
+            'IxxPSF': 'slot_PsfShape_xx',
+            'Iyy': 'slot_Shape_yy',
+            'IyyPSF': 'slot_PsfShape_yy',
+            'Ixy': 'slot_Shape_xy',
+            'IxyPSF': 'slot_PsfShape_xy',
+            'mag': 'mag',
+            'magerr': 'mag_err',
+            'apFlux': (convert_flux_to_nanoJansky, 'slot_ApFlux_{}'.format(FLUX)),
+            'apFluxErr': (convert_flux_to_nanoJansky, 'slot_ApFlux_{}{}'.format(FLUX, ERR)),
+            'apFlux_flag': 'slot_ApFlux_flag',
+            'psFlux': (convert_flux_to_nanoJansky, 'slot_PsfFlux_{}'.format(FLUX)),
+            'psFluxErr': (convert_flux_to_nanoJansky, 'slot_PsfFlux_{}{}'.format(FLUX, ERR)),
+            'psFlux_flag': 'slot_PsfFlux_flag',
+            'psNdata': 'slot_PsfFlux_area',
+            'psf_fwhm_pixels': (
+                lambda xx, yy, xy: 2.355 * (xx * yy - xy * xy) ** 0.25,
+                'slot_PsfShape_xx',
+                'slot_PsfShape_yy',
+                'slot_PsfShape_xy',
+            ),
+            # There are no 'slot_*' values for the extendedness and blendedness
+            # in the Run 1.2i processing (as of 2019-03-05)
+            'extendedness': 'base_ClassificationExtendedness_value',
+            'blendedness': 'base_Blendedness_abs_{}'.format(FLUX),
+        }
+
+        not_good_flags = (
+            'base_PixelFlags_flag_edge',
+            'base_PixelFlags_flag_interpolatedCenter',
+            'base_PixelFlags_flag_saturatedCenter',
+            'base_PixelFlags_flag_crCenter',
+            'base_PixelFlags_flag_bad',
+            'base_PixelFlags_flag_suspectCenter',
+            'base_PixelFlags_flag_clipped',
+        )
+
+        modifiers['good'] = (create_basic_flag_mask,) + not_good_flags
+        modifiers['clean'] = (
+            create_basic_flag_mask,
+            'deblend_skipped',
+        ) + not_good_flags
+
+        return modifiers
+
+    @staticmethod
+    def _generate_info_dict(meta_path):
+        """Creates a 2d dictionary with information for each homogenized quantity
+
+        Args:
+            meta_path (path): Path of yaml config file with object meta data
+            bands     (list): List of photometric bands as strings
+
+        Returns:
+            Dictionary of the form
+                {<homonogized value (str)>: {<meta value (str)>: <meta data>}, ...}
+        """
+
+        with open(meta_path, 'r') as ofile:
+            base_dict = yaml.load(ofile)
+
+        info_dict = dict()
+        for quantity, info_list in base_dict.items():
+            quantity_info = dict(
+                description=info_list[0],
+                unit=info_list[1],
+                in_GCRbase=info_list[2],
+                in_DPDD=info_list[3]
+            )
+
+        info_dict[quantity] = quantity_info
+
+        return info_dict
+
+    def _get_quantity_info_dict(self, quantity, default=None):
+        """Return a dictionary with descriptive information for a quantity
+
+        Returned information includes a quantity description, quantity units, whether
+        the quantity is defined in the DPDD, and if the quantity is available in GCRbase.
+
+        Args:
+            quantity   (str): The quantity to return information for
+            default (object): Value to return if no information is available (default None)
+
+        Returns:
+            A dictionary with information about the provided quantity
+        """
+
+        return self._quantity_info_dict.get(quantity, default)
+
+    def _generate_datasets(self):
+        """Return viable data sets from all files in self.base_dir
+
+        Returns:
+            A list of ObjectTableWrapper(<file path>, <key>) objects
+            for all files and keys
+        """
+        datasets = list()
+        for fname in sorted(os.listdir(self.base_dir)):
+            if not self._filename_re.match(fname):
+                continue
+
+            file_path = os.path.join(self.base_dir, fname)
+            try:
+                fh = self._open_hdf5(file_path)
+
+            except (IOError, OSError):
+                warnings.warn('Cannot access {}; skipped'.format(file_path))
+                continue
+
+            for key in fh:
+                if self._groupname_re.match(key.lstrip('/')):
+                    datasets.append(ObjectTableWrapper(fh, key, self._schema))
+                    continue
+
+                warn_msg = 'incorrect group name "{}" in {}; skipped this group'
+                warnings.warn(warn_msg.format(os.path.basename(file_path), key))
+
+        return datasets
+
+    @staticmethod
+    def _generate_schema_from_yaml(schema_path):
+        """Return a dictionary of columns based on schema in YAML file
+
+        Args:
+            schema_path (string): <file path> to schema file.
+
+        Returns:
+            The columns defined in the schema.
+            A dictionary of {<column_name>: {'dtype': ..., 'default': ...}, ...}
+
+        Warns:
+            If one or more column names are repeated.
+        """
+
+        with open(schema_path, 'r') as schema_stream:
+            schema = yaml.load(schema_stream)
+
+        if schema is None:
+            warn_msg = 'No schema can be found in schema file {}'
+            warnings.warn(warn_msg.format(schema_path))
+
+        return schema
+
+    @staticmethod
+    def _generate_schema_from_datafiles(datasets):
+        """Return the native schema for given datasets
+
+        Args:
+            datasets (list): A list of tuples (<file path>, <key>)
+
+        Returns:
+            A dict of schema ({col_name: {'dtype': dtype}}) found in all data sets
+        """
+
+        schema = {}
+        for dataset in datasets:
+            schema.update(dataset.native_schema)
+
+        return schema
+
+    def generate_schema_yaml(self, overwrite=False):
+        """
+        Generate the schema from the datafiles and write as a yaml file.
+        This function write the schema yaml file to the schema location specified for the catalog.
+        One needs to set `overwrite=True` to overwrite an existing schema file.
+        """
+        if self._schema_path and os.path.isfile(self._schema_path):
+            if not overwrite:
+                raise RuntimeError('Schema file `{}` already exists! Set `overwrite=True` to overwrite.'.format(self._schema_path))
+            warnings.warn('Overwriting schema file `{0}`, which is backed up at `{0}.bak`'.format(self._schema_path))
+            shutil.copyfile(self._schema_path, self._schema_path + '.bak')
+
+        schema = self._generate_schema_from_datafiles(self._datasets)
+
+        for col, schema_this in schema.items():
+            if schema_this['dtype'] == 'bool' and (
+                    col.endswith('_flag_bad') or col.endswith('_flag_noGoodPixels')):
+                schema_this['default'] = True
+
+        with open(self._schema_path, 'w') as schema_stream:
+            yaml.dump(schema, schema_stream)
+
+    def clear_cache(self):
+        """Empty the catalog reader cache and frees up memory allocation"""
+
+        for dataset in self._datasets:
+            dataset.clear_cache()
+
+    def _open_parquet(self, file_path):
+        """Return the a Pandas DataFrame of a Parquet file
+
+        Args:
+            file_path (str): The path of the desired file
+
+        Return:
+            The cached file handle
+        """
+
+        if (file_path not in self._file_handles or
+                not self._file_handles[file_path].is_open):
+            self._file_handles[file_path] = pd.HDFStore(file_path, 'r')
+
+        return self._file_handles[file_path]
+
+    def _generate_native_quantity_list(self):
+        """Return a set of native quantity names as strings"""
+
+        return set(self._schema).union(self._native_filter_quantities)
+
+    def _iter_native_dataset(self, native_filters=None):
+        # pylint: disable=C0330
+        for dataset in self._datasets:
+            if (native_filters is None):
+                yield dataset.get
+                if not self.use_cache:
+                    dataset.clear_cache()
