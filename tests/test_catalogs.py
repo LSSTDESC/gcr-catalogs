04753ec4b3ff90add381cf71bbcb76c2b8b0274f
Yao-Yuan Mao <yymao.astro@gmail.com>
2019-05-30 15:37:08 -0400

    better assert

--- tests/test_catalogs.py
+++ tests/test_catalogs.py
@@ -9,7 +9,8 @@ all_catalogs = list(GCRCatalogs.get_available_catalogs(include_default_only=Fals
 @pytest.mark.parametrize("catalog", all_catalogs)
 def test_load_catalog_and_quantities(catalog):
     cat = GCRCatalogs.load_catalog(catalog)
-    q = cat.list_all_quantities()[:1] + cat.list_all_native_quantities()[:1]
-    for data in cat.get_quantities(q, return_iterator=True):
-        assert data is not None
+    qs = cat.list_all_quantities()[:1] + cat.list_all_native_quantities()[:1]
+    assert qs
+    for data in cat.get_quantities(qs, return_iterator=True):
+        assert all(q in data for q in qs)
         break
