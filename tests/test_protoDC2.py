5997cb8850ae2c137ba8821912b0317d60484ce5
Yao-Yuan Mao <yymao.astro@gmail.com>
2019-05-30 15:22:16 -0400

    use pytest for protoDC2 test

--- tests/test_protoDC2.py
+++ tests/test_protoDC2.py
@@ -1,31 +1,29 @@
-from __future__ import unicode_literals, absolute_import, print_function
-import numpy as np
+"""
+test_protoDC2.py
+"""
+import pytest
 from astropy.cosmology import FlatLambdaCDM
-from GCR import GCRQuery
 import GCRCatalogs
 
+# pylint: disable=redefined-outer-name
+@pytest.fixture(scope='module')
+def protoDC2():
+    gc = GCRCatalogs.load_catalog('protoDC2')
+    print('cosmology: {}'.format(gc.cosmology))
+    print('version = {}'.format(gc.get_catalog_info('version')))
+    return gc
 
-gc = GCRCatalogs.load_catalog('protoDC2')
-print('cosmology: {}'.format(gc.cosmology))
-print('version = {}'.format(gc.get_catalog_info('version')))
+def test_lightcone(protoDC2):
+    assert protoDC2.lightcone, 'Must be lightcone'
 
-def test_lightcone():
-    assert gc.lightcone, 'Must be lightcone'
+def test_cosmology(protoDC2):
+    assert isinstance(protoDC2.cosmology, FlatLambdaCDM), 'Must be FlatLambdaCDM'
 
-def test_cosmology():
-    assert isinstance(gc.cosmology, FlatLambdaCDM), 'Must be FlatLambdaCDM'
-    
-def test_info():
-    for q in sorted(gc.list_all_native_quantities()):
-        assert gc.get_quantity_info(q), '{} does not have quantity_info'.format(q)
+def test_info(protoDC2):
+    for q in protoDC2.list_all_native_quantities():
+        assert protoDC2.get_quantity_info(q), '{} does not have quantity_info'.format(q)
 
-def test_angles():
-    units = ['radians', 'degrees', 'arcsecond']
-    angular_quantities = [q for q in sorted(gc.list_all_native_quantities()) if gc.get_quantity_info(q, default={}).get('unit') in units]
-    for q in angular_quantities:
-        print('{}: min={:13.4g} max={:13.4g}'.format(q, np.min(gc[q]), np.max(gc[q])))
-
-def test_skyarea():
-    assert hasattr(gc,'sky_area'), 'Must have sky area'
-    assert isinstance(gc.sky_area, float), 'Must be float'
-    print('sky area = {}'.format(gc.sky_area)) 
+def test_skyarea(protoDC2):
+    assert hasattr(protoDC2,'sky_area'), 'Must have sky area'
+    assert isinstance(protoDC2.sky_area, float), 'Must be float'
+    print('sky area = {}'.format(protoDC2.sky_area))
