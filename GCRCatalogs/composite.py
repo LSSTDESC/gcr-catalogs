2440517dbf877597bacc59a243b4d5f310933a02
Yao-Yuan Mao <yymao.astro@gmail.com>
2019-05-31 15:01:49 -0400

    allow composite config overwrite

--- GCRCatalogs/composite.py
+++ GCRCatalogs/composite.py
@@ -3,7 +3,7 @@ composite reader
 """
 import warnings
 from GCR import CompositeCatalog, MATCHING_FORMAT, MATCHING_ORDER
-from .register import load_catalog, load_catalog_from_config_dict
+from .register import load_catalog, load_catalog_from_config_dict, available_catalogs
 
 class CompositeReader(CompositeCatalog):
     def __init__(self, **kwargs):
@@ -11,10 +11,11 @@ class CompositeReader(CompositeCatalog):
         identifiers = []
         methods = []
         for catalog_dict in kwargs['catalogs']:
-            if 'subclass_name' in catalog_dict:
-                catalog = load_catalog_from_config_dict(catalog_dict)
+            catalog_name = catalog_dict.get('catalog_name')
+            if catalog_name in available_catalogs:
+                catalog = load_catalog(catalog_name, catalog_dict)
             else:
-                catalog = load_catalog(catalog_dict['catalog_name'])
+                catalog = load_catalog_from_config_dict(catalog_dict)
             instances.append(catalog)
             identifiers.append(catalog_dict.get('catalog_name'))
             method = catalog_dict.get('matching_method', 'MATCHING_FORMAT')
