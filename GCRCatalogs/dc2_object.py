4c0806d2a9879a3f7d9c0a25d4ecc9e92fcbbfab
Yao-Yuan Mao <yymao.astro@gmail.com>
2019-07-17 15:43:12 +0200

    update DC2ObjectParquetCatalog

--- GCRCatalogs/dc2_object.py
+++ GCRCatalogs/dc2_object.py
@@ -13,7 +13,9 @@ import pandas as pd
 import yaml
 from GCR import BaseGenericCatalog
 
-__all__ = ['DC2ObjectCatalog']
+from .dc2_dm_catalog import DC2DMCatalog
+
+__all__ = ['DC2ObjectCatalog', 'DC2ObjectParquetCatalog']
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 FILE_PATTERN = r'(?:merged|object)_tract_\d+\.hdf5$'
@@ -629,3 +631,57 @@ class DC2ObjectCatalog(BaseGenericCatalog):
             # pylint: disable=attribute-defined-outside-init
             self._len = sum(len(dataset) for dataset in self._datasets)
         return self._len
+
+
+class DC2ObjectParquetCatalog(DC2DMCatalog):
+    r"""DC2 Object (Parquet) Catalog reader
+
+    Parameters
+    ----------
+    base_dir          (str): Directory of data files being served, required
+    filename_pattern  (str): The optional regex pattern of served data files
+    is_dpdd          (bool): File are already in DPDD-format.  No translation.
+
+    Attributes
+    ----------
+    base_dir          (str): The directory of data files being served
+
+    Notes
+    -----
+    """
+    # pylint: disable=too-many-instance-attributes
+    FILE_DIR = FILE_DIR
+    FILE_PATTERN = r'object_tract_\d+\.parquet$'
+    META_PATH = META_PATH
+
+    _generate_modifiers = DC2ObjectCatalog._generate_modifiers
+
+    def _subclass_init(self, **kwargs):
+
+        # hack to skip the call of `_generate_modifiers` in the base class
+        # TODO: fix this some day
+        super()._subclass_init(**dict(kwargs, is_dpdd=True))
+
+        self.pixel_scale = float(kwargs.get('pixel_scale', 0.2))
+
+        if kwargs.get('is_dpdd'):
+            self._quantity_modifiers = {col: None for col in self._columns}
+        else:
+            # A slightly crude way of checking for version of schema to have modelfit mag
+            # A future improvement will be to explicitly store version information in the datasets
+            # and just rely on that versioning.
+            has_modelfit_mag = any(col.endswith('_modelfit_mag') for col in self._columns)
+
+            if any(col.endswith('_fluxSigma') for col in self._columns):
+                dm_schema_version = 1
+            elif any(col.endswith('_fluxErr') for col in self._columns):
+                dm_schema_version = 2
+            elif any(col == 'base_Blendedness_abs_instFlux' for col in self._columns):
+                dm_schema_version = 3
+            else:
+                dm_schema_version = 4
+
+            bands = [col[0] for col in self._columns if len(col) == 5 and col.endswith('_mag')]
+
+            self._quantity_modifiers = self._generate_modifiers(
+                self.pixel_scale, bands, has_modelfit_mag, dm_schema_version)
