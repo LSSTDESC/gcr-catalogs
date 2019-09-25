LSSTDESC/gcr-catalogs

commit cc8bbc14854a08f9df0dbf59af40a6a06d2cd892

Use convert_dm_ref_zp_flux_to_mag for HDF5 files instead of lambdas.

GCRCatalogs/dc2_object.py
@@ -404,14 +404,11 @@ class DC2ObjectCatalog(BaseGenericCatalog):
             if not has_modelfit_mag:
                 # The zp=27.0 is based on the default calibration for the coadds
                 # as specified in the DM code.  It's correct for Run 1.1p.
-                modifiers['mag_{}_cModel'.format(band)] = (
-                    lambda x: -2.5 * np.log10(x) + 27.0,
-                    '{}_modelfit_CModel_{}'.format(band, FLUX),
-                )
-                modifiers['magerr_{}_cModel'.format(band)] = (
-                    lambda flux, err: (2.5 * err) / (flux * np.log(10)),
-                    '{}_modelfit_CModel_{}'.format(band, FLUX),
-                    '{}_modelfit_CModel_{}{}'.format(band, FLUX, ERR),
+                modifiers['mag_{}_cModel'.format(band)] = (convert_dm_ref_zp_flux_to_mag,
+                                                           'mag_{}_cModel'.format(band))
+                modifiers['magerr_{}_cModel'.format(band)] = (convert_flux_err_to_mag_err,
+                                                              '{}_modelfit_CModel_{}'.format(band, FLUX),
+                                                              '{}_modelfit_CModel_{}{}'.format(band, FLUX, ERR),
                 )
                 modifiers['snr_{}_cModel'.format(band)] = (
                     np.divide,

commit 7eb19a584056d45081aa3691bdba3e21582648af

Fix the definition of mag_{}_cModel for HDF5 DC2ObjectCatalog
This bug was introduced in cc8bbc14 as part of #369.
There was a bug where mag_{}_cmodel was defined baesd on mag_{}_cModel

This should have only affected Run 1.1p and HSC.
because it was triggered if the modelfit mags were not already calculated
and only for the default (HDF5) catalogs.  The Parquet class was fine.

GCRCatalogs/dc2_object.py
@@ -405,7 +405,7 @@ class DC2ObjectCatalog(BaseGenericCatalog):
                 # The zp=27.0 is based on the default calibration for the coadds
                 # as specified in the DM code.  It's correct for Run 1.1p.
                 modifiers['mag_{}_cModel'.format(band)] = (convert_dm_ref_zp_flux_to_mag,
-                                                           'mag_{}_cModel'.format(band))
+                                                           '{}_modelfit_CModel_{}'.format(band, FLUX))
                 modifiers['magerr_{}_cModel'.format(band)] = (convert_flux_err_to_mag_err,
                                                               '{}_modelfit_CModel_{}'.format(band, FLUX),
                                                               '{}_modelfit_CModel_{}{}'.format(band, FLUX, ERR),