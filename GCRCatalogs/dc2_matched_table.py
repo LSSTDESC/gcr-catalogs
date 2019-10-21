8d246ba87f98266791322e0bb834dbb6c439dc2d
evevkovacs <kovacs@anl.gov>
2019-10-21 12:02:03 -0700

    fix travis issues

--- GCRCatalogs/dc2_matched_table.py
+++ GCRCatalogs/dc2_matched_table.py
@@ -17,7 +17,7 @@ class DC2MatchedTable(BaseGenericCatalog):
     def _subclass_init(self, **kwargs):
 
         """
-        DC2MatchedTable reader, inherited from BaseGenericCatalog class 
+        DC2MatchedTable reader, inherited from BaseGenericCatalog class
         """
         self._filename = kwargs.get('filename', None)
         if not self._filename:
@@ -32,30 +32,30 @@ class DC2MatchedTable(BaseGenericCatalog):
 
         # check matched table quantities
         with fits.open(self._filename) as hdul:
-            cols = hdul[1].columns.names
+            hdulist = list(hdul)[1]
+            cols = hdulist.columns.names
             if not object_id in cols or not match_flag in cols or not is_star in cols or not truth_id in cols:
                 print('Matching table does not have minimal expected columns')
                 return
 
-            self._galaxy_match_mask = (~hdul[1].data[is_star]) & (hdul[1].data[match_flag])
+            self._galaxy_match_mask = (~hdulist.data[is_star]) & (hdulist.data[match_flag])
             self._matched_galaxy_count = np.count_nonzero(self._galaxy_match_mask)
-            self._star_match_mask = (hdul[1].data[is_star]) & (hdul[1].data[match_flag])
+            self._star_match_mask = (hdulist.data[is_star]) & (hdulist.data[match_flag])
             self._matched_star_count = np.count_nonzero(self._star_match_mask)
-            
+
         # modify native quantities
         self._quantity_modifiers = {}
         modified_quantity_list = [c for c in cols if not is_star in c and not match_flag in c]
         for q in modified_quantity_list:
-            self._quantity_modifiers[q + '_galaxy'] =  (lambda x: ma.MaskedArray(x, mask=self._galaxy_match_mask), q)
-            self._quantity_modifiers[q + '_star'] =  (lambda x: ma.MaskedArray(x, mask=self._star_match_mask), q)
-
+            self._quantity_modifiers[q + '_galaxy'] = (lambda x: ma.MaskedArray(x, mask=self._galaxy_match_mask), q)
+            self._quantity_modifiers[q + '_star'] = (lambda x: ma.MaskedArray(x, mask=self._star_match_mask), q)
 
     def _generate_native_quantity_list(self):
         """
         Return an iterable of all native quantity names.
         """
         with fits.open(self._filename) as hdul:
-            return hdul[1].columns.names
+            return list(hdul)[1].columns.names
 
         
     def _iter_native_dataset(self, native_filters=None):
@@ -66,5 +66,5 @@ class DC2MatchedTable(BaseGenericCatalog):
         assert not native_filters, '*native_filters* is not supported'
         with fits.open(self._filename) as hdul:
             def native_quantity_getter(native_quantity):
-                return hdul[1].data[native_quantity]
+                return list(hdul)[1].data[native_quantity]
             yield native_quantity_getter
