a8e2094e7e22e87ac216542c5fcfbf3ec991da87
Joanne Bogart <jrb@slac.stanford.edu>
2020-02-13 16:02:02 -0800

    probably temporary patch to test_reader_modules.py

--- tests/test_reader_modules.py
+++ tests/test_reader_modules.py
@@ -4,6 +4,10 @@ test_reader_modules.py
 import pytest
 import GCRCatalogs
 
+# Cheat so this test can be run anywhere
+if GCRCatalogs.register._ROOT_DIR is None:
+    GCRCatalogs.register._ROOT_DIR = '/something'
+
 all_readers = GCRCatalogs.register.get_reader_list()
 
 @pytest.mark.parametrize('reader', all_readers)
