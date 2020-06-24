91c10dd775c3b522943b9dc62f258c5eacee04a8
Joanne Bogart <jrb@slac.stanford.edu>
2020-06-24 16:40:56 -0700

    Use parquet.ParquetFileWrapper

--- GCRCatalogs/dc2_dm_catalog.py
+++ GCRCatalogs/dc2_dm_catalog.py
@@ -15,6 +15,7 @@ import numpy as np
 import pyarrow.parquet as pq
 import yaml
 from GCR import BaseGenericCatalog
+from .parquet import ParquetFileWrapper
 
 from .utils import first
 
@@ -87,51 +88,6 @@ def create_basic_flag_mask(*flags):
         out &= (~flag)
 
     return out
-
-
-class ParquetFileWrapper():
-    def __init__(self, file_path, info=None):
-        self.path = file_path
-        self._handle = None
-        self._columns = None
-        self._info = info or dict()
-
-    @property
-    def handle(self):
-        if self._handle is None:
-            self._handle = pq.ParquetFile(self.path)
-        return self._handle
-
-    def close(self):
-        self._handle = None
-
-    def __len__(self):
-        return int(self.handle.scan_contents)
-
-    def __contains__(self, item):
-        return item in self.columns
-
-    def read_columns(self, columns, as_dict=False):
-        d = self.handle.read(columns=columns).to_pandas()
-        if as_dict:
-            return {c: d[c].values for c in columns}
-        return d
-
-    @property
-    def info(self):
-        return dict(self._info)
-
-    def __getattr__(self, name):
-        if name not in self._info:
-            raise AttributeError('Attribute {} does not exist'.format(name))
-        return self._info[name]
-
-    @property
-    def columns(self):
-        if self._columns is None:
-            self._columns = [col for col in self.handle.schema.to_arrow_schema().names
-                             if re.match(r'__\w+__$', col) is None]
-        return list(self._columns)
 
 
 class DC2DMCatalog(BaseGenericCatalog):
@@ -299,14 +255,17 @@ class DC2DMCatalog(BaseGenericCatalog):
         Overloading this so that we can query the database backend
         for multiple columns at once
         """
-        return native_quantity_getter.read_columns(list(native_quantities_needed), as_dict=True)
+        return native_quantity_getter.read_columns_row_group(list(native_quantities_needed),
+                                                             as_dict=True)
 
     def _iter_native_dataset(self, native_filters=None):
         for dataset in self._datasets:
             if (native_filters is not None and
                     not native_filters.check_scalar(dataset.info)):
                 continue
-            yield dataset
+            for i in range(dataset.num_row_groups):
+                dataset.current_row_group = i
+                yield dataset
 
     def __len__(self):
         if self._len is None:
