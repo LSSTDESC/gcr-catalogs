c595edf7c34abad968af59f6a3480b7d6ef3e650
Yao-Yuan Mao <yymao.astro@gmail.com>
2020-12-12 08:43:52 -0500

    move to method _generate_matchdc2_quantity_modifiers

--- GCRCatalogs/dc2_truth_match.py
+++ GCRCatalogs/dc2_truth_match.py
@@ -56,20 +56,7 @@ class DC2TruthMatchCatalog(DC2DMTractCatalog):
             raise ValueError("Reader options `as_object_addon` and `as_truth_table` cannot both be set to True.")
 
         if self._as_matchdc2_schema:
-            # To recreate column names in https://github.com/fjaviersanchez/MatchDC2/blob/master/python/matchDC2.py
-            flux_cols = [k for k in self._quantity_modifiers if k.startswith("flux_") and k.endswith("_noMW")]
-            self._quantity_modifiers = {
-                "truthId": (lambda i, t: np.where(t < 3, i, "-1").astype(np.int64), "id", "truth_type"),
-                "objectId": "match_objectId",
-                "is_matched": "is_good_match",
-                "is_star": (lambda t: t > 1, "truth_type"),
-                "ra": "ra",
-                "dec": "dec",
-                "redshift_true": "redshift",
-                "dist": "match_sep",
-            }
-            for col in flux_cols:
-                self._quantity_modifiers["mag_" + col.split("_")[1] + "_lsst"] = (_flux_to_mag, col)
+            self._quantity_modifiers = self._generate_matchdc2_quantity_modifiers(list(self._quantity_modifiers))
             return
 
         flux_cols = [k for k in self._quantity_modifiers if k.startswith("flux_")]
@@ -121,3 +108,35 @@ class DC2TruthMatchCatalog(DC2DMTractCatalog):
             else:
                 self._len = sum(len(dataset) for dataset in self._datasets)
         return self._len
+
+    @staticmethod
+    def _generate_matchdc2_quantity_modifiers(native_columns):
+        """
+        To recreate column names in dc2_matched_table.py
+        cf. https://github.com/fjaviersanchez/MatchDC2/blob/master/python/matchDC2.py
+        """
+
+        quantity_modifiers = {
+            "truthId": (lambda i, t: np.where(t < 3, i, "-1").astype(np.int64), "id", "truth_type"),
+            "objectId": "match_objectId",
+            "is_matched": "is_good_match",
+            "is_star": (lambda t: t > 1, "truth_type"),
+            "ra": "ra",
+            "dec": "dec",
+            "redshift_true": "redshift",
+            "dist": "match_sep",
+        }
+
+        for col in native_columns:
+            if col.startswith("flux_") and col.endswith("_noMW"):
+                quantity_modifiers["mag_" + col.split("_")[1] + "_lsst"] = (_flux_to_mag, col)
+
+        for col in list(quantity_modifiers):
+            if col in ("is_matched", "is_star"):
+                continue
+            quantity_modifiers[col + "_galaxy"] = (lambda d, t, m: np.ma.array(d, mask=((t == 1) & m)), col, "truth_type", "is_good_match")
+            quantity_modifiers[col + "_star"] = (lambda d, t, m: np.ma.array(d, mask=((t == 2) & m)), col, "truth_type", "is_good_match")
+        quantity_modifiers['galaxy_match_mask'] = (lambda t, m: (t == 1) & m, "truth_type", "is_good_match")
+        quantity_modifiers['star_match_mask'] = (lambda t, m: (t == 2) & m, "truth_type", "is_good_match")
+
+        return quantity_modifiers
